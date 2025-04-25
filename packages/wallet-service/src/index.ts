daf0963d9ae31739fd110d3ad5fe03de678c6634
nutcas3 <mauricee423@gmail.com>
2025-04-25 16:37:50 +0300

    feat: Add wallet service module and index exports

---
+++ packages/wallet-service/src/index.ts
@@ -0,0 +1,8 @@
+// Export legacy direct imports
+export * from './generateWallet';
+export * from './encryptWallet';
+export * from './walletStore';
+
+// Export NestJS module and service
+export * from './wallet.service';
+export * from './wallet-service.module';
