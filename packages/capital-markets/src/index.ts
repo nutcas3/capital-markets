65c99a0504646db45ce5e02214575d31d87d0b53
nutcas3 <mauricee423@gmail.com>
2025-04-30 14:11:49 +0300

    feat: Export modules
    This commit exports several modules, making their functionality available for use in other parts of the application:
    
    -   Exports `usd-engine`.
    -   Exports `synthetic-token-engine`.
    -   Exports `risk-exposure-module`.
    -   Exports `yield-curve-aggregator`.
    -   Exports `capital-markets.module`.

---
+++ packages/capital-markets/src/index.ts
@@ -0,0 +1,5 @@
+export * from './usd-engine';
+export * from './synthetic-token-engine';
+export * from './risk-exposure-module';
+export * from './yield-curve-aggregator';
+export * from './capital-markets.module';
